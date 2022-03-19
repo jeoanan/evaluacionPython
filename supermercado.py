6ff1f44c5889a193e4095253ed2d86115855872f
206 <206@academicos.cesde.edu.co>
2022-03-18 19:04:25 -0500

    NTV003 Se crea el menu del supermercado

---
+++ supermercado.py
@@ -0,0 +1,12 @@
+print("********************************************")
+print("Bienvenido a superMercados la gente de Bien!")
+print("********************************************")
+print("Ingresa un valor de nuestro menu para continuar")
+print("Ingresa 1 para agregar un nuevo producto:")
+print("Ingresa 2 para consultar nuestros productos:")
+print("Ingresa 3 para editar un producto productos:")
+print("Ingresa 4 para eliminar un producto:")
+
+menuIngresado=int(input(""))
+
+""" while(menuIngresado <= 0 or ) """
